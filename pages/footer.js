1b8eab99c2f20f655557b5d96dae8b63ce04a1b3
Jane <42087697+Iris1e27@users.noreply.github.com>
2022-09-05 17:59:52 -0400

    Update footer.js

--- pages/footer.js
+++ pages/footer.js
@@ -2,8 +2,8 @@ class Footer extends React.Component {
   render() {
     return (
       <footer>
-        <a href="/Student-Course-Selction-System/pages/index.html">Home</a> | <a href="/Student-Course-Selction-System/pages/log.html">Log</a> |{" "}
-        <a href="/Student-Course-Selction-System/pages/qa.html">FAQ</a> | <a href="/Student-Course-Selction-System/pages/ack.html">Ack</a> | <a href="/Student-Course-Selction-System/pages/other.html">Other</a>
+        <a href="/index.html">Home</a> | <a href="/log.html">Log</a> |{" "}
+        <a href="/qa.html">FAQ</a> | <a href="/ack.html">Ack</a> | <a href="/other.html">Other</a>
         <br />
         Created by{" "}
         <a href="https://blog.csdn.net/zjt1027?type=blog">Jingtian Zhang</a>
